ec84b3ae7966810f520956f11357f9acbe77c0e5
makehoney <makehoney3@gmail.com>
2018-08-26 04:35:59 +0900

    add comments on IntToHex func

--- utils.go
+++ utils.go
@@ -8,10 +8,12 @@ import (
 
 func IntToHex(num int64) []byte {
 	buff := new(bytes.Buffer)
+	// num을 big endian 순서로 buff에 binary 형식으로 쓴다.
 	err := binary.Write(buff, binary.BigEndian, num)
 	if err != nil {
 		log.Panic(err)
 	}
 
+	// buff에 쓰인 binary 데이터를 byte형식(16진수 형식)으로 리턴
 	return buff.Bytes()
 }
